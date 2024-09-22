b1c8f66e6ade8ab4caf84dac89b684457f9318c3
albertopiyo <aopiyo28@gmail.com>
2024-09-22 08:59:37 +0300

    Changes done

--- voting.py
+++ voting.py
@@ -21,28 +21,6 @@ consumer = Consumer(conf | {
 
 producer = SerializingProducer(conf)
 
-# def consume_messages():
-#     result = []
-#     consumer.subscribe(['candidates_topic'])
-#     try:
-#         while True:
-#             msg = consumer.poll(timeout=1.0)
-#             if msg is None:
-#                 continue
-#             elif msg.error():
-#                 if msg.error().code() == KafkaError._PARTITION_EOF:
-#                     continue
-#                 else:
-#                     print(msg.error())
-#                     break
-#             else:
-#                 result.append(json.loads(msg.value().decode('utf-8')))
-#                 if len(result) == 3:
-#                     return result
-
-#             time.sleep(5)
-#     except KafkaException as e:
-#         print(e)
 
 if __name__ == "__main__":
     
